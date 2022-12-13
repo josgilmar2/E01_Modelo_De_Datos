83040de9e07f9375146328300421606b811507c1
josgilmar2 <josgilmar2@gmail.com>
2022-12-13 18:15:13 +0100

    Se hace el orphan removal y el cascade

--- ejercicio02/src/main/java/com/salesianostriana/dam/jpa/ejercicio02/model/CursoOnline.java
+++ ejercicio02/src/main/java/com/salesianostriana/dam/jpa/ejercicio02/model/CursoOnline.java
@@ -23,7 +23,7 @@ public class CursoOnline {
     @JoinColumn(name = "profesor_id", foreignKey = @ForeignKey(name = "FK_CURSOONLINE_PROFESOR"))
     private Profesor profesor;
 
-    @OneToMany(mappedBy = "cursoOnline", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "cursoOnline", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     @ToString.Exclude
     private List<Video> videos = new ArrayList<>();
